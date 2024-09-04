7b0a47fc45bbeec8ae78dc68540e021ea5b2b1f7
rivdomdev <rivdomdev@gmail.com>
2024-09-04 13:27:59 +0200

    app created

---
+++ src/views/MainView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-screen bg-slate-100 font-mono">
+        <h1 class="text-blue-400 text-xl text-center p-4">RAMDOM IMAGES APP WITH VUE</h1>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+</script>
